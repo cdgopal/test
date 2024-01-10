ed6b3e90d5f1bc62ef05f15d2e81653210e9c059
Gopal Das <148480024+cdgopal@users.noreply.github.com>
2024-01-10 22:00:16 +0530

    Update App.java

--- src/main/java/com/example/TestDemo/App.java
+++ src/main/java/com/example/TestDemo/App.java
@@ -10,7 +10,5 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "Hello World! Java" );
-        for(int i=0;i<=10;i++){
-            System.out.println( "Hello World!" +i );
-    }
+       
 }
